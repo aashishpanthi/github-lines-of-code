97c78b7f69dd39f87d9463b437472c251602067c
Aashish Panthi <aashishpanthi11@gmail.com>
2022-09-23 13:43:08 +0545

    complete video and login modal popup

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,13 @@
 import styles from "./styles/hero.module.css";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import { useState } from "react";
+import Modal from "../components/Modal";
+import { Button, Box, Typography } from "@mui/material";
 
 const Hero = () => {
+  const [videoOpen, setvideoOpen] = useState(false);
+  const [loginOpen, setLoginOpen] = useState(false);
+
   return (
     <div className={styles.hero}>
       <h1 className={styles.hero__title}>Count Your Lines of Code</h1>
@@ -11,11 +17,44 @@ const Hero = () => {
       </p>
 
       <div className={styles.hero__buttons}>
-        <button className={styles.loginBtn}>Get Started Now</button>
-        <button className={styles.videoBtn}>
+        <button onClick={() => setLoginOpen(true)} className={styles.loginBtn}>
+          Get Started Now
+        </button>
+        <button onClick={() => setvideoOpen(true)} className={styles.videoBtn}>
           <PlayArrowIcon /> <span>Watch Video</span>
         </button>
       </div>
+      <Modal open={loginOpen} setOpen={setLoginOpen}>
+        <Box sx={{ p: 4, textAlign: "center", maxWidth: "95%" }}>
+          <Typography id="transition-modal-title" variant="h6" component="h2">
+            Login with GitHub
+          </Typography>
+          <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+            Please allow us to access your private and public repositories to
+            get your stats better.
+          </Typography>
+
+          <Button
+            variant="outlined"
+            color="success"
+            size="large"
+            style={{ marginTop: "10px" }}
+          >
+            Continue with GitHub
+          </Button>
+        </Box>
+      </Modal>
+      <Modal open={videoOpen} setOpen={setvideoOpen}>
+        <iframe
+          width="550"
+          height="315"
+          src="https://www.youtube.com/embed/lEi_XBg2Fpk"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </Modal>
     </div>
   );
 };
---
+++ src/components/Modal.js
@@ -0,0 +1,39 @@
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "background.paper",
+  border: "none",
+  boxShadow: 24,
+  p: 1,
+  borderRadius: 1,
+};
+function CustomModal({ open, setOpen, children }) {
+  const handleClose = () => setOpen(false);
+
+  return (
+    <Modal
+      aria-labelledby="transition-modal-title"
+      aria-describedby="transition-modal-description"
+      open={open}
+      onClose={handleClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={open}>
+        <Box sx={style}>{children}</Box>
+      </Fade>
+    </Modal>
+  );
+}
+
+export default CustomModal;
