73d5765cb1faf8468365f7cda97d1cdc04a64c76
Aashish Panthi <aashishpanthi11@gmail.com>
2022-09-23 14:13:23 +0545

    complete footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,46 @@
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import styles from "./styles/footer.module.css";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className={styles.appInfo}>
+        <a href="https://youtube.com/" className={styles.appLink}>
+          Demo video
+        </a>
+        <a href="https://blog.aashish-panthi.com.np" className={styles.appLink}>
+          Full guide
+        </a>
+        <a href="https://github.com/aashishpanthi" className={styles.appLink}>
+          Source code
+        </a>
+      </div>
+
+      <div className={styles.authorInfo}>
+        <p>
+          Developed by{" "}
+          <a href="https://aashishpanthi.info.np">Aashish Panthi</a>
+        </p>
+        <div className={styles.authorLinks}>
+          <a href="" title="Find on GitHub">
+            <GitHubIcon />
+          </a>
+          <a href="" title="Find on Twitter">
+            <TwitterIcon />
+          </a>
+          <a href="" title="Connect on Linkedin">
+            <LinkedInIcon />
+          </a>
+          <a href="" title="Follow on Instagram">
+            <InstagramIcon />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
