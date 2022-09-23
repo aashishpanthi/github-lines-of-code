59ff773aecbdb7e27660ca60efe56c1ff76b8383
Aashish Panthi <aashishpanthi11@gmail.com>
2022-09-23 12:49:33 +0545

    navbar completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+import styles from "./styles/navbar.module.css";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className={styles.navbar}>
+      <Link className={styles.navbar__logo} to="/">
+        <img src="/logo192.png" alt="logo" />
+        <span>Lines of Code</span>
+      </Link>
+      <div className={styles.navbar__links}>
+        <Link to="/">Home</Link>
+        <button className={styles.navButton}>Login</button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
