b926ad0f415df66a45ddaa8808a908d2fb3bbb8e
Aashish Panthi <aashishpanthi11@gmail.com>
2022-09-26 00:13:28 +0545

    chart edited

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,21 +4,31 @@ import DoughNutChart from "./DoughNutChart";
 const Card = ({ data }) => {
   const { username, photo, totalLines, publicRepo, privateRepo, languages } =
     data;
+
+  let formatter = Intl.NumberFormat("en", {
+    notation: "compact",
+  });
+
+  const formattedTotalLines = formatter.format(totalLines);
+  const formattedPublicRepo = formatter.format(publicRepo);
+  const formattedPrivateRepo = formatter.format(privateRepo);
+
   return (
     <div className={styles.card}>
       <div className={styles.card__main}>
         <div className={styles.main_chart}>
           <DoughNutChart data={[publicRepo, privateRepo]} />
+          <div className={styles.totalLines}>{formattedTotalLines}</div>
         </div>
         <div className={styles.main_stats}>
           <h3>
-            Total lines of code: <span>{totalLines}</span>
+            Total lines of code: <span>{formattedTotalLines}</span>
           </h3>
-          <h3>
-            Public Repository: <span>{publicRepo}</span>
+          <h3 style={{ color: "#fe3533" }}>
+            Public Repository: <span>{formattedPublicRepo}</span>
           </h3>
-          <h3>
-            Private Repository: <span>{privateRepo}</span>
+          <h3 style={{ color: "#bc4500" }}>
+            Private Repository: <span>{formattedPrivateRepo}</span>
           </h3>
         </div>
       </div>
--- src/components/DoughNutChart.js
+++ src/components/DoughNutChart.js
@@ -5,17 +5,40 @@ const DoughNutChart = ({ data }) => {
   return (
     <Doughnut
       data={{
-        labels: ["LOC in Public Repos", "LOC in Private Repos"],
+        labels: ["Public Repos", "Private Repos"],
         datasets: [
           {
             data: data,
-            backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)"],
+            backgroundColor: ["#fe3533", "#bc4500"],
             hoverOffset: 1,
           },
         ],
       }}
       options={{
+        cutout: "55%",
         plugins: {
+          tooltip: {
+            callbacks: {
+              label: function (context) {
+                let label = context.label || "";
+
+                if (label) {
+                  label += ": ";
+                }
+
+                let formatter = Intl.NumberFormat("en", {
+                  notation: "compact",
+                });
+
+                label += formatter.format(
+                  context.formattedValue.replace(/,/g, "")
+                );
+
+                console.log(label);
+                return label;
+              },
+            },
+          },
           legend: {
             display: false,
           },
